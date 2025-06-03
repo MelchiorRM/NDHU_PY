954eeaca0707d0e19190daf75cc905750b7cc1af
MelchiorRM <melchiorrem001@gmail.com>
2025-06-04 02:16:34 +0800

    files correction with gitignore

--- decode_url.py
+++
@@ -1,5 +0,0 @@
-import base64
-
-encoded = "CBwQAhoeEgoyMDI0LTEyLTAxagcIARIDSkZLcgcIARIDTEFYGh4SCjIwMjQtMTItMDVqBwgBEgNMQVhyBwgBEgNKRktAAUgBcAGCAQsI____________AZgBAQ"
-decoded = base64.urlsafe_b64decode(encoded + "==")
-print(decoded) 
